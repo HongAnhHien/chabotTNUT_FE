73bb9d8fb105c0e6db36647e66036cceb72a3ddd
cuongcoki <trandinhcuongfpt2022@gmail.com>
2026-06-22 17:05:27 +0700

    fix sv cònix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['chatbotfe.girc.edu.vn'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
